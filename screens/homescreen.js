507a54abeb051d4a39c6ff93752f3aff5eafa27c
Sarath Vadlamannati <sarathvadlamannati@coherendz.com>
2020-06-13 01:47:46 +0530

    - feat: home screen done

--- screens/homescreen.js
+++ screens/homescreen.js
@@ -1,12 +1,55 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  SafeAreaView,
+  FlatList,
+  Dimensions,
+  TouchableOpacity
+} from "react-native";
+import { items } from "../data";
+import Item from "../components/item";
+
+const { width, height } = Dimensions.get("window");
 
 class HomeScreen extends Component {
+  renderItem = item => {
+    const { navigation } = this.props;
+
+    return (
+      <TouchableOpacity onPress={() => navigation.navigate("Cart")}>
+        <View
+          style={{
+            paddingHorizontal: 10,
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "space-between"
+          }}
+        >
+          <Item
+            imgSrc={item.imgPath}
+            width={width}
+            name={item.name}
+            price={item.price}
+          />
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Home Screen</Text>
-      </View>
+      <SafeAreaView contentContainerStyle={styles.container}>
+        <FlatList
+          data={items}
+          renderItem={({ item }) => this.renderItem(item)}
+          keyExtractor={item => item.id.toString()}
+          numColumns={2}
+        />
+        <Text>Hi</Text>
+      </SafeAreaView>
     );
   }
 }
@@ -14,8 +57,7 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
+    flex: 1
+    // padding: 10
   }
 });
